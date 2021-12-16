437e4f0adc62ed57497d593cce3541c4ef68dc31
Joseph Coulter <joeycoulter@gmail.com>
2021-12-15 17:39:59 -0800

    Animation and text fills

--- my-portfolio/pages/index.js
+++ my-portfolio/pages/index.js
@@ -5,10 +5,10 @@ import { motion, AnimatePresence } from "framer-motion";
 import React, { useState } from "react";
 import Modal from "../components/Modal";
 import Accordion from "../components/Accordion";
-import Nav from '../components/Nav'
-import Ring from "../components/Ring"
-import introVector from '../public/lineVector.svg'
-import nextBtn from '../public/nextBtn.svg'
+import Nav from "../components/Nav";
+import Ring from "../components/Ring";
+import introVector from "../public/lineVector.svg";
+import nextBtn from "../public/nextBtn.svg";
 
 export default function Home() {
   const [isToggled, setToggle] = useState(false);
@@ -26,41 +26,42 @@ export default function Home() {
       {/* <button onClick={() => setToggle(true)}>Contact</button> */}
 
       <main className={styles.main}>
-        <Accordion title={'my info'} className={styles.info}/>
-        <div className={styles.titleCtnr}>
-          <h1 className={styles.title}>Joe Cee</h1>
-        </div>
+        <Accordion title={"my info"} className={styles.info} />
+        <motion.div className={styles.titleCtnr}>
+          <motion.h1
+            whileHover={{
+              scale: [1, 0.8],
+              backgroundSize: "400%",
+              transition: {
+                duration: 0.75,
+                ease: "bounceOut",
+              },
+            }}
+            className={styles.title}
+          >
+            Joe Cee
+          </motion.h1>
+        </motion.div>
         {/* <Modal isToggled={isToggled} setToggle={setToggle}>
           <div className={styles.title} animate={{opacity: isToggled }}> contact forms </div>
         </Modal> */}
-        
-        
-          <div className={styles.ringCtnr}>
-            <Ring />
-          </div>
-          <section className={styles.introTextCtnr}>
-            <h2 className={styles.line1}>Coding simply</h2>
-            <div className={styles.introVector}>
-              <Image
-                src={introVector}
-                alt="A squiggly line graphic"
-                
-              />
-            </div>
-            <h2 className={styles.line2}>for the web</h2>
-            <h2 className={styles.line3}>&</h2>
-            <h2 className={styles.line4}>b e y o n d</h2>
-        
-          </section>  
-          <div className={styles.nextCtnr}>
-            <Image 
-              src={nextBtn}
-              alt="animated button to go to next page"
-            />
-          </div>
-      </main>
 
-  
+        <div className={styles.ringCtnr}>
+          <Ring />
+        </div>
+        <section className={styles.introTextCtnr}>
+          <h2 className={styles.line1}>Coding simply</h2>
+          <div className={styles.introVector}>
+            <Image src={introVector} alt="A squiggly line graphic" />
+          </div>
+          <h2 className={styles.line2}>for the web</h2>
+          <h2 className={styles.line3}>&</h2>
+          <h2 className={styles.line4}>b e y o n d</h2>
+        </section>
+        <div className={styles.nextCtnr}>
+          <Image src={nextBtn} alt="animated button to go to next page" />
+        </div>
+      </main>
     </div>
   );
 }
